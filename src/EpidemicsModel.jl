15365ac4b1b702cdf76dad140fa9e89b41dd636b
dknatan <natandominko@gmail.com>
2023-12-20 10:48:37 +0100

    add plotting function

--- src/EpidemicsModel.jl
+++ src/EpidemicsModel.jl
@@ -18,7 +18,14 @@ function get_evolution_SIRV(u0, p; Δt=0.1, T=100.0)
     ds = ContinuousDynamicalSystem(SIRV, u0, p)
     traj, timearray = trajectory(ds, T; Δt)
     print("Integrated the dynamical system to time T = $(T)")
-    return nothing
+    return traj, timearray
+end
+
+function plot_evolution_SIRV(u0, p, traj, timearray; Δt=0.1, T=100.0)
+    using Plots
+    labels = ["Susceptible", "Infected", "Recovered", "Vaccinated"] #[traj[1,:] traj[2,:] traj[3,:] traj[4,:]]
+    plot(timearray, traj, title= "Model at β = $(p[1]), γ = $(p[2]), v = $(p[3])", label=labels, linewidth=3)
+    
 end
 
 
