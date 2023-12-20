1cc3e788fc4fd979eb75bbc2eae61732cd136207
dknatan <natandominko@gmail.com>
2023-12-20 12:13:45 +0100

    include basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "EpidemicsModel.jl" begin
     # Write your tests here.
+    @test EpidemicsModel.SIRV([10.0, 10.0, 10.0, 10.0], [0.0, 0.0, 0.0], "k") == SVector{4}(0.0, 0.0, 0.0, 0.0)
+    @test EpidemicsModel.SIRV([0.0, 0.0, 0.0, 0.0], [10.0, 10.0, 10.0], "k") == SVector{4}(0.0, 0.0, 0.0, 0.0)
 end
