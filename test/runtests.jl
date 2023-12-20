83e98db3497e418510ab81fb1eddd03a1392bf36
dknatan <natandominko@gmail.com>
2023-12-20 12:27:39 +0100

    change

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using Test
 
 @testset "EpidemicsModel.jl" begin
     # Write your tests here.
-    @test EpidemicsModel.SIRV([0.0, 0.0, 0.0, 0.0], [10.0, 10.0, 10.0], "k") == EpidemicsModel.SIRV([10.0, 10.0, 10.0, 10.0], [0.0, 0.0, 0.0], "k")
+    @test EpidemicsModel.SIRV([0.0, 0.0, 10.0, 10.0], [10.0, 10.0, 10.0], "k") == EpidemicsModel.SIRV([10.0, 10.0, 10.0, 10.0], [0.0, 0.0, 0.0], "k")
 end
